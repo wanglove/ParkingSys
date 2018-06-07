7b8583b9df55d46ab2debe7da4d6c895629e6da2
wanglove <wanglove@126.com>
2018-06-07 14:35:59 +0800

    设置数据库链接池初始和最大的链接数

--- ParkingSysApi/config.py
+++ ParkingSysApi/config.py
@@ -1,4 +1,9 @@
 #数据库配置
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@localhost:3306/parking?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+# 数据库链接池初始和最大的链接数
+SQLALCHEMY_POOL_SIZE = 5
+SQLALCHEMY_MAX_OVERFLOW = 5
+
 DEBUG = True
